92b68df4b8de04d085e67621fb1597072312f0d8
yigitqweqew <66117596+yigitqweqew@users.noreply.github.com>
2023-12-10 12:06:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+import random
+
+characters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "=", ",", ".", ";", ":", "'", "\"", "/", "\\"]
+
+while True:
+    input("Press enter to generate password : ")
+
+    randomCharacter1 = random.choice(characters)
+    randomCharacter2 = random.choice(characters)
+    randomCharacter3 = random.choice(characters)
+    randomCharacter4 = random.choice(characters)
+    randomCharacter5 = random.choice(characters)
+    randomCharacter6 = random.choice(characters)
+    randomCharacter7 = random.choice(characters)
+    randomCharacter8 = random.choice(characters)
+    randomCharacter9 = random.choice(characters)
+    randomCharacter10 = random.choice(characters)
+    randomCharacter11 = random.choice(characters)
+    randomCharacter12 = random.choice(characters)
+    randomCharacter13 = random.choice(characters)
+    randomCharacter14 = random.choice(characters)
+    randomCharacter15 = random.choice(characters)
+    randomCharacter16 = random.choice(characters)
+    randomCharacter17 = random.choice(characters)
+    randomCharacter18 = random.choice(characters)
+    randomCharacter19 = random.choice(characters)
+    randomCharacter20 = random.choice(characters)
+    randomCharacter21 = random.choice(characters)
+    randomCharacter22 = random.choice(characters)
+    randomCharacter23 = random.choice(characters)
+    randomCharacter24 = random.choice(characters)
+
+    password = randomCharacter1 + randomCharacter2 + randomCharacter3 + randomCharacter4 + randomCharacter5 + randomCharacter6 + randomCharacter7 + randomCharacter8 + randomCharacter9 + randomCharacter10 + randomCharacter11 + randomCharacter12 + randomCharacter13 + randomCharacter14 + randomCharacter15 + randomCharacter16 + randomCharacter17 + randomCharacter18 + randomCharacter19 + randomCharacter20 + randomCharacter21 + randomCharacter22 + randomCharacter23 + randomCharacter24
+
+    print(password)
